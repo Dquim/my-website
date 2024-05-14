a203db186298b38f0ba25c363c2d1fdd208fe2c8
Dquim <knowdontcha@gmail.com>
2024-05-14 11:47:49 -0400

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	new file:   src/assets/images/Galaxy.jpg 	new file:   src/assets/images/LI-In-Bug.png 	new file:   src/assets/images/LI-Logo.png 	new file:   src/assets/images/github-mark-white.png 	new file:   src/assets/images/github-mark.png

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,4 @@ export class AppComponent {
   title = 'my-website';
   allLines = '• Diagnose and resolve intricate hardware and software challenges through meticulous troubleshooting, ensuring seamless functionality and optimal performance.' + ' ' + '• Identify and resolve complex network issues, implementing strategic solutions to enhance stability and ensure uninterrupted connectivity.' + ' ' + ' • Manage and optimize Active Directory environments, demonstrating adeptness in user account management and group policy configuration.';
   treloarHeisel = '• Utilized Nintex for data import/export between Excel and Applied Epic, streamlining data consolidation.' + '• Implemented a Microsoft Excel application for recruiters to collect data on potential recruits.' + '• Collaborated with team members to efficiently complete daily tasks and projects.' + '• Created queries and cross-referenced data between Epic and HubSpot to prevent duplicates.' + '• Reformatted computers with the company’s in-house software stack.'
-  
-  longText = 'Fuck';
 }
